5607ee2edd2ca9ec9dbdeaba899631be2ed457dc
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-11-16 16:01:30 +0530

    Navigation and Flat List

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import ListView from './src/List';
 
-export default function App() {
+const STACK = createNativeStackNavigator();
+const HomeScreen = ({ navigation }) => (
+  <View>
+    <Text>Home Screen</Text>
+    <Button title="Student List" onPress={() => navigation.navigate("list")} />
+  </View>
+);
+
+const ProfileScreen = () => (
+  <View>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+export default function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <STACK.Navigator initialRouteName="home">
+        <STACK.Screen name="home" component={HomeScreen} />
+        <STACK.Screen name="profile" component={ProfileScreen} />
+        <STACK.Screen name="list" component={ListView} />
+      </STACK.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +37,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    fontSize: 44, 
+    color: '#458798', 
+    letterSpacing: 4
+  },
+  subHeading: {
+    fontSize: 30, 
+    color: '#a0a0a0'
+  },
+  image: {
+    padding: 12,
+    borderRadius: 25,
+    width: 300,
+    height: 256,
+    marginTop: 24,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 300,
+      height: -256
+    },
+    shadowOpacity: 0.7,
+    shadowRadius: 1,
+    // elevation: 4r
+  }
 });
---
+++ src/List.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+
+interface STDDATA {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+}
+
+const STUDENT_DATA: any[] = [
+  { id: 1, name: 'Gurlivleen', email: 'gurlivleen@example.com', age: 21 },
+  { id: 2, name: 'Prabhjot', email: 'prabhjot@example.com', age: 20 },
+  { id: 3, name: 'Anmol', email: 'anmol@example.com', age: 17 },
+];
+
+const ListItem = ({ data }) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.heading1}>{ data.name }</Text>
+      <Text style={styles.subHeading}>{ data.email }</Text>
+      <Text style={styles.subHeading}>{ data.age }</Text>
+    </View>
+  );
+}
+
+const ListView = () => {
+  const render = ({ item }) => <ListItem data={item} />
+
+  return (
+    <View>
+      <FlatList data={STUDENT_DATA} renderItem={render} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    // width: '12rem',
+    // height: '5rem',
+    flex: 1,
+    backgroundColor: '#fff',
+    borderColor: "#a0a0a0",
+    borderWidth: 4,
+    borderRadius: 12,
+    margin: 10,
+    padding: 10
+  },
+  heading1: {
+    fontSize: 44,
+  },
+  subHeading: {
+    fontSize: 24,
+    color: '#a0a0a0',
+    marginTop: 2
+  }
+})
+
+export default ListView
